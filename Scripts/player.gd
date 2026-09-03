9d89ab8a0990d7f6aa2bf81f80daa71426d0acaa
HuynhTrungTin52 <thuongtrunghuynh11@gmail.com>
2026-09-04 04:19:52 +0700

    Refactoring player.gd and attempting to connect with StageSystem
    Additionally, added EntityStats as a resource and added the player's stats and enemy's stats

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,19 +1,42 @@
 extends CharacterBody2D
+class_name Player
+
+signal end_turn
+
 
 const tile_size: Vector2 = Vector2(16,16)
 var sprite_node_pos_tween: Tween
 
-func _physics_process(delta: float) -> void:
+@export var stats: EntityStats
+var current_ap: int =0
+var is_my_turn: bool = false
+
+
+func action() -> void:
+	if stats:
+		current_ap=stats.ap
+		
+	is_my_turn=true
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not is_my_turn or current_ap <= 0 :
+		return
+	var dir = Vector2.ZERO
 	if Input.is_action_just_pressed("ui_up") and !$up.is_colliding():
-		_move(Vector2(0,-1))
+		dir = Vector2(0,-1)
 	elif Input.is_action_just_pressed("ui_down") and !$down.is_colliding():
-		_move(Vector2(0,1))
+		dir = Vector2(0,1)
 	elif Input.is_action_just_pressed("ui_left") and !$left.is_colliding():
-		_move(Vector2(-1,0))
+		dir = Vector2(-1,0)
 	elif Input.is_action_just_pressed("ui_right") and !$right.is_colliding():
-		_move(Vector2(1,0))
+		dir = Vector2(1,0)
+		
+	if dir != Vector2.ZERO:
+		get_viewport().set_input_as_handled() 
+		_move(dir)
 		
 func _move(dir:Vector2):
+	current_ap-=1
 	global_position += dir * tile_size
 	$Sprite2D.global_position -= dir * tile_size
 	
@@ -23,3 +46,12 @@ func _move(dir:Vector2):
 	sprite_node_pos_tween = create_tween()
 	sprite_node_pos_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	sprite_node_pos_tween.tween_property($Sprite2D,"global_position",global_position,0.185).set_trans(Tween.TRANS_SINE)
+	await  sprite_node_pos_tween.finished
+	
+	if current_ap <=0:
+		is_my_turn=false
+		end_turn.emit()
+		
+func die() -> void:
+	BusStage.player_died.emit()
+	queue_free()
