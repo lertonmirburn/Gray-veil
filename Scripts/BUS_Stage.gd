74a4cf3e3f00b9b9f90c653dfdddfc631e46556c
lertonmirburn <lertonmirburn@email.com>
2026-09-11 23:53:48 +0700

    added fog scene

--- Scripts/BUS_Stage.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-signal player_died()
-signal enemy_die(enemy:Enemy)
-signal player_end_turn()
-signal enemy_finish_turn(enemy:Enemy)
-signal battle_won
-signal battle_lost
-var current_stage:int=0
